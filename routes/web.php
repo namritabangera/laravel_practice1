afaa53fff321dfb93ba413871eeceb6fb082d282
amrita2084 <amrita2084@yahoo.com>
2026-06-16 07:19:46 +0530

    my first laravel practice set

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function(){
+
+return "Hello Laravel";
+});
+
+Route::get('/about',function(){
+    return "This is my first laravel application";
+});
+
+Route::get('/user/{name}', function($name){
+
+return "Welcome ".$name;
+});
+
+Route::get('/products', [ProductController::class,'index']);
